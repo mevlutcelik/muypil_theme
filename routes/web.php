126fecf9e04d7e29d9210eb3be593cf1bcc7f92d
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-06-21 10:36:39 +0300

    Update web.php
    Routes have been updated. Made simpler.

--- routes/web.php
+++ routes/web.php
@@ -16,39 +16,15 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
     /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
-    Route::get('/', function () {
-        return view('theme2.pages.home.index');
-    })->name('home');
-    Route::get(LaravelLocalization::transRoute('theme2-global.routes-comments'), function () {
-        return view('theme2.pages.comments.index');
-    })->name('comments');
-    Route::get(LaravelLocalization::transRoute('theme2-global.routes-policies'), function () {
-        return view('theme2.pages.policies.index');
-    })->name('policies');
-
-    Route::get(LaravelLocalization::transRoute('theme2-global.routes-product'), function () {
-        return view('theme2.pages.product.index');
-    })->name('product');
-
-    Route::get(LaravelLocalization::transRoute('theme2-global.routes-pricing'), function () {
-        return view('theme2.pages.pricing.index');
-    })->name('pricing');
-
-    Route::get(LaravelLocalization::transRoute('theme2-global.routes-agencies'), function () {
-        return view('theme2.pages.agencies.index');
-    })->name('agencies');
-
-    Route::get(LaravelLocalization::transRoute('theme2-global.routes-login'), function () {
-        return view('theme2.pages.login.index');
-    })->name('login');
-
-    Route::get(LaravelLocalization::transRoute('theme2-global.routes-reset-password'), function () {
-        return view('theme2.pages.reset-password.index');
-    })->name('reset-password');
-
-    Route::get(LaravelLocalization::transRoute('theme2-global.routes-careers'), function () {
-        return view('theme2.pages.careers.index');
-    })->name('careers');
+    Route::view('/', 'theme2.pages.home.index')->name('home');
+    Route::view(LaravelLocalization::transRoute('theme2-global.routes-comments'), 'theme2.pages.comments.index')->name('comments');
+    Route::view(LaravelLocalization::transRoute('theme2-global.routes-policies'), 'theme2.pages.policies.index')->name('policies');
+    Route::view(LaravelLocalization::transRoute('theme2-global.routes-product'), 'theme2.pages.product.index')->name('product');
+    Route::view(LaravelLocalization::transRoute('theme2-global.routes-pricing'), 'theme2.pages.pricing.index')->name('pricing');
+    Route::view(LaravelLocalization::transRoute('theme2-global.routes-agencies'), 'theme2.pages.agencies.index')->name('agencies');
+    Route::view(LaravelLocalization::transRoute('theme2-global.routes-login'), 'theme2.pages.login.index')->name('login');
+    Route::view(LaravelLocalization::transRoute('theme2-global.routes-reset-password'), 'theme2.pages.reset-password.index')->name('reset-password');
+    Route::view(LaravelLocalization::transRoute('theme2-global.routes-register'), 'theme2.pages.careers.index')->name('careers');
 
     Route::get(LaravelLocalization::transRoute('theme2-global.routes-register'), function () {
         return 'register sayfası';
@@ -56,14 +32,9 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
 
     // Dashboard
     Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
-        Route::get('/', function () {
-            return view('theme2.pages.dashboard.home.index');
-        })->name('home');
-        Route::get('trackers', function () {
-            return view('theme2.pages.dashboard.trackers.index');
-        })->name('trackers');
-        Route::get('packets', function () {
-            return view('theme2.pages.dashboard.packets.index');
-        })->name('packets');
+        Route::view('/', 'theme2.pages.dashboard.home.index')->name('home');
+        Route::view('trackers', 'theme2.pages.dashboard.trackers.index')->name('trackers');
+        Route::view('packets', 'theme2.pages.dashboard.packets.index')->name('packets');
+        Route::view('packets/checkout', 'theme2.pages.dashboard.packets.checkout')->name('packets-checkout');
     });
 });
