0d1848b0ab6db1a803c2515356446bd256bac6a2
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-06-27 13:39:03 +0300

    Update header-cards.blade.php

--- resources/views/theme2/sections/header-cards.blade.php
+++ resources/views/theme2/sections/header-cards.blade.php
@@ -2,7 +2,7 @@
     <div class="container pb-15 pb-md-3">
         <div class="row gx-md-5 gy-5 mt-n19 mb-0">
             <div class="col-md-6 col-xl-3">
-                <div class="card shadow-lg">
+                <div class="card shadow-lg h-100">
                     <div class="card-body">
                         <div style="display:flex;align-items: center;">
                             <img src="{{asset('assets2/img/icons/2704312.png')}}" style="width: auto;height: 1.75rem;object-fit:cover;" class="svg-inject icon-svg icon-svg-sm solid-mono text-fuchsia mb-3" alt="" />
@@ -16,7 +16,7 @@
             </div>
             <!--/column -->
             <div class="col-md-6 col-xl-3">
-                <div class="card shadow-lg">
+                <div class="card shadow-lg h-100">
                     <div class="card-body">
                         <div style="display: flex;align-items: center;">
                             <img src="{{asset('assets2/img/icons/1055653.png')}}" style="width: auto;height: 1.75rem;object-fit:cover;" class="svg-inject icon-svg icon-svg-sm solid-mono text-violet mb-3" alt="" />
@@ -30,7 +30,7 @@
             </div>
             <!--/column -->
             <div class="col-md-6 col-xl-3">
-                <div class="card shadow-lg">
+                <div class="card shadow-lg h-100">
                     <div class="card-body">
                         <div style="display: flex;align-items: center;">
                             <img src="{{asset('assets2/img/icons/1792155.png')}}" style="width: auto;height: 1.75rem;object-fit:cover;" class="svg-inject icon-svg icon-svg-sm solid-mono text-orange mb-3" alt="" />
@@ -44,7 +44,7 @@
             </div>
             <!--/column -->
             <div class="col-md-6 col-xl-3">
-                <div class="card shadow-lg">
+                <div class="card shadow-lg h-100">
                     <div class="card-body">
                         <div style="display: flex;align-items: center;">
                             <img src="{{asset('assets2/img/icons/1021182.png')}}" style="width: auto;height: 1.75rem;object-fit:cover;" class="svg-inject icon-svg icon-svg-sm solid-mono text-green mb-3" alt="" />
