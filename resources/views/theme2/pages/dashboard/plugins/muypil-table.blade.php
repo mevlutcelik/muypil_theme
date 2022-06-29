37b3a15eef7b3de6ec835a5ee0fa2ccc8c2171ab
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-06-29 14:27:43 +0300

    Update muypil-table.blade.php

--- resources/views/theme2/pages/dashboard/plugins/muypil-table.blade.php
+++ resources/views/theme2/pages/dashboard/plugins/muypil-table.blade.php
@@ -11,6 +11,7 @@
             width: 100%;
             overflow-x: auto;
             overflow-y: auto;
+            scrollbar-color: rgba(0,0,0,0.15) transparent;
             height: calc((var(--vh, 1vh) * 100) - 380px);
             position: relative;
         }
